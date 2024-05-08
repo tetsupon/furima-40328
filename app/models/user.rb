51581cc53dea5af6e388e5e3cdd56e6f529d5602
tetsupon <te.ka.20230305@gmail.com>
2024-05-08 21:21:38 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    
+   validates :nickname, uniqueness: true
+
+   validates :email,    uniqueness: true
+   # 重複しないように
+
+   validates :password, format: { with: /\A[a-zA-Z]+\z/ }
+   # 半角英数字混合
+
+   validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+   validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+   # 全角ひらがな・カタカナ・漢字
+
+   validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+   validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+   # 全角カタカナ
+
+   validates :birth_day, presence: true
+  end 
 end
