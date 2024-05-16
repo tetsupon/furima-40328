b0bb383cc2393aab464e7228a55b082835bf6357
tetsupon <te.ka.20230305@gmail.com>
2024-05-16 20:56:26 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,12 @@ class Item < ApplicationRecord
 
   # active_storageとのアソシエーション（items・active_storage_blobsテーブルを関連付け）
   has_one_attached :image
+
+  # アクティブハッシュとのアソシエーション
+  belongs_to :product_category
+  belongs_to :product_condition
+  belongs_to :delivery_charge
+  belongs_to :shipping_area
+  belongs_to :shipping_day
+  
 end
