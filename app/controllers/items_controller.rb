8fcccb76fc0492c17a666f831a77f2b0db0524a8
tetsupon <te.ka.20230305@gmail.com>
2024-05-19 12:58:21 +0900

    商品情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,19 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :product_name, :product_description, :product_category_id, :product_condition_id, :delivery_charge_id, :shipping_area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 
 end
