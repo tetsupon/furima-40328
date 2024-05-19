3cd6ffe62c7e8ea29771fa82c8080deb4c4dac77
tetsupon <te.ka.20230305@gmail.com>
2024-05-19 11:27:55 +0900

    ログイン状態の場合のみ、商品出品ページへ遷移できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
+
 end
