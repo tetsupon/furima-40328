c2fdafff8be859952a6bc0efb0849c8b765d6e68
tetsupon <te.ka.20230305@gmail.com>
2024-05-22 19:09:23 +0900

    Java Scriptの修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () => {
+window.addEventListener('turbo:render', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
