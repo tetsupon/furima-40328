e887389cb8558d4b35021f11ca744dd37654d4c8
tetsupon <te.ka.20230305@gmail.com>
2024-05-29 20:24:35 +0900

    「変更する」「もどる」ボタンが正常に機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
+  before_action :set_item, only: [:show, :edit, :update]
+
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,14 +22,17 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
   
   def update
+    if @item.update(item_params)
+      redirect_to item_path(item_params)
+    else
+      render 'edit'
+    end
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def destroy
@@ -37,6 +42,10 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :product_name, :product_description, :product_category_id, :product_condition_id, :delivery_charge_id, :shipping_area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
