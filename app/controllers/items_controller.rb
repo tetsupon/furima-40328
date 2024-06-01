2847a213c064fb743620c0343edb858f6d950115
tetsupon <te.ka.20230305@gmail.com>
2024-06-01 14:50:42 +0900

    Revert "削除機能"
    This reverts commit f7f3fb9220cf95b01ed8befc2caba97138bfdcf4.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update]
 
 
   def index
@@ -39,14 +39,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.user_id == current_user.id
-      if @item.destroy
-        redirect_to root_path
-      else
-        render "items/show"
-      end
-    end
-  end
+  end  
 
   private
 
