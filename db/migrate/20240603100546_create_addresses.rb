725e0f606461acc3a550548f6f19315e138054fc
tetsupon <te.ka.20230305@gmail.com>
2024-06-03 19:17:21 +0900

    addressマイグレーションフファイル記述

--- db/migrate/20240603100546_create_addresses.rb
+++ db/migrate/20240603100546_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :addresses do |t|
-
+      t.references :buyer,         null: false, foreign_key: true
+      t.string     :post_code,      null: false
+      t.integer    :shipping_area_id, null: false
+      t.string     :municipality,          null: false
+      t.string     :street_address,         null: false
+      t.string     :building_name
+      t.string     :phone_number,  null: false
       t.timestamps
     end
   end
