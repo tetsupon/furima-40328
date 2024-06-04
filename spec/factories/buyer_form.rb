0eee159c7bb8f8c15a9397fd1240f56c4aa3d2c7
tetsupon <te.ka.20230305@gmail.com>
2024-06-04 20:10:27 +0900

    FactoryBotの作成

---
+++ spec/factories/buyer_form.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :buyer_form do
+    user_id { Faker::Number.non_zero_digit }
+    item_id { Faker::Number.non_zero_digit }
+    post_code { Faker::Number.decimal_part(digits: 3) + '-' + Faker::Number.decimal_part(digits: 4) }
+    shipping_area_id { Faker::Number.between(from: 1, to: 47) }
+    municipality { Faker::Address.city }
+    street_address { Faker::Address.street_address }
+    building_name { Faker::Address.building_number }
+    phone_number { Faker::Number.decimal_part(digits: 11) }
+  end
+end
