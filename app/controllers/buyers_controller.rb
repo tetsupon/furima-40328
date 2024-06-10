aab10894c1907c67c608489989b8d29376718e1c
tetsupon <te.ka.20230305@gmail.com>
2024-06-10 20:05:31 +0900

    修正３

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -15,6 +15,7 @@ class BuyersController < ApplicationController
      @buyerform.save
      redirect_to root_path
     else
+     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
      render :index, status: :unprocessable_entity
     end
   end
